8efecc03fc46e8518627a501c206b138a6937867
SkorobagatiyMykola <nick1980457@gmail.com>
2023-12-17 20:24:44 +0200

    Added service

--- src/main/java/ua/skorobahatyi/onlineshopping/repository/AccountRepository.java
+++ src/main/java/ua/skorobahatyi/onlineshopping/repository/AccountRepository.java
@@ -3,5 +3,6 @@ package ua.skorobahatyi.onlineshopping.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.skorobahatyi.onlineshopping.model.Account;
 
+
 public interface AccountRepository extends JpaRepository<Account,Integer> {
 }
---
+++ src/main/java/ua/skorobahatyi/onlineshopping/service/AccountService.java
@@ -0,0 +1,6 @@
+package ua.skorobahatyi.onlineshopping.service;
+
+@Service
+public class AccountService {
+
+}
