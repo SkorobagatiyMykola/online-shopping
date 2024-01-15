285f022acae8580cfc5be1346c824bde246c031c
SkorobagatiyMykola <nick1980457@gmail.com>
2024-01-15 16:11:25 +0200

    Added entity

--- src/main/java/ua/skorobahatyi/onlineshopping/model/User.java
+++ src/main/java/ua/skorobahatyi/onlineshopping/model/User.java
@@ -1,13 +1,19 @@
 package ua.skorobahatyi.onlineshopping.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 import java.time.LocalDate;
 
+
+@Entity
+@Table (name="user")
 public class User {
     private int id;
     private String name;
     private String surname;
     private int age;
-    private String email;
-    private LocalDate created_at;
-    private LocalDate updated_at;
+//    private String email;
+//    private LocalDate created_at;
+//    private LocalDate updated_at;
 }
