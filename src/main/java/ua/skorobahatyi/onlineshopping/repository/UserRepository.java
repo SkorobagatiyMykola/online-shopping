a4ffdda5a9fda34f12501b8887c5c602ff0ee7ee
SkorobagatiyMykola <nick1980457@gmail.com>
2024-01-15 16:27:56 +0200

    Changed Repository for User

--- src/main/java/ua/skorobahatyi/onlineshopping/repository/UserRepository.java
+++ src/main/java/ua/skorobahatyi/onlineshopping/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package ua.skorobahatyi.onlineshopping.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ua.skorobahatyi.onlineshopping.model.User;
 
-public interface UserRepository extends CrudRepository<User,Integer> {
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
 }
