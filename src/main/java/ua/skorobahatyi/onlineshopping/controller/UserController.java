6e34f01ec7111bd166c27907df49984adfcbc64e
SkorobagatiyMykola <nick1980457@gmail.com>
2024-01-15 16:16:36 +0200

    Added config files

--- src/main/java/ua/skorobahatyi/onlineshopping/controller/UserController.java
+++ src/main/java/ua/skorobahatyi/onlineshopping/controller/UserController.java
@@ -1,4 +1,8 @@
 package ua.skorobahatyi.onlineshopping.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/users")
 public class UserController {
 }
