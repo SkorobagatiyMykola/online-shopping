2b05a74950454744a1ac838cce74fd018463d9c3
SkorobagatiyMykola <nick1980457@gmail.com>
2024-01-20 22:48:42 +0200

    Improved code for Account

--- src/main/java/ua/skorobahatyi/onlineshopping/model/Account.java
+++ src/main/java/ua/skorobahatyi/onlineshopping/model/Account.java
@@ -1,5 +1,12 @@
 package ua.skorobahatyi.onlineshopping.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "account")
 public class Account {
+    @Id
     private int userId;
 }
--- src/main/java/ua/skorobahatyi/onlineshopping/repository/AccountRepository.java
+++ src/main/java/ua/skorobahatyi/onlineshopping/repository/AccountRepository.java
@@ -1,8 +1,10 @@
 package ua.skorobahatyi.onlineshopping.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ua.skorobahatyi.onlineshopping.model.Account;
 
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account,Integer> {
 }
