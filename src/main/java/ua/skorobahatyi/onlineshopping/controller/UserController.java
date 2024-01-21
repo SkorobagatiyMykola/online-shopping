c1ed6b8c37176ab4f3be73fd579aea1fa677266c
SkorobagatiyMykola <nick1980457@gmail.com>
2024-01-21 19:33:09 +0200

    Improved UserController

--- src/main/java/ua/skorobahatyi/onlineshopping/controller/UserController.java
+++ src/main/java/ua/skorobahatyi/onlineshopping/controller/UserController.java
@@ -1,5 +1,7 @@
 package ua.skorobahatyi.onlineshopping.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController("/users")
 public class UserController {
+    private static final Logger logger = LogManager.getLogger(UserController.class);
 
     private final UserService userService;
 
@@ -24,18 +27,19 @@ public class UserController {
     }
 
     @ResponseStatus(CREATED)
-    @PostMapping(consumes = APPLICATION_JSON_VALUE)
+    @PostMapping()
+    //@PostMapping(consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<HttpStatus> createUser(@RequestBody User userBody) {
-        //logger.debug("POST /user");
+        logger.debug("POST /user");
         User user = userService.createUser(userBody);
 
-        //logger.info("User {} has been created.", user.getId());
+        logger.info("User {} has been created.", user.getId());
         return new ResponseEntity<>(CREATED);
     }
 
     @GetMapping()
     public ResponseEntity<List<User>> getAllUsers() {
-        //logger.debug("GET /users, all users");
+        logger.debug("GET /users, all users");
         List<User> users = userService.getUsers();
 
         return new ResponseEntity<>(users, HttpStatus.OK);
