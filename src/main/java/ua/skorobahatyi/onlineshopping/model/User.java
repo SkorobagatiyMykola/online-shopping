93b3f6bde881a37632cccfe13afe3ed38f1fabaa
SkorobagatiyMykola <nick1980457@gmail.com>
2024-01-22 16:54:47 +0200

    Updeted User

--- src/main/java/ua/skorobahatyi/onlineshopping/model/User.java
+++ src/main/java/ua/skorobahatyi/onlineshopping/model/User.java
@@ -3,12 +3,14 @@ package ua.skorobahatyi.onlineshopping.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 import java.time.LocalDate;
 
 
 @Entity
 @Table (name="user")
+@Data
 public class User {
     @Id
     private int id;
